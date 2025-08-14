f33b23195b63d401a93efd39e1ab7cbb77d35cc5
luis escobar <luisescobar74628191@gmail.com>
2025-08-13 20:39:08 -0500

    Empezando el sofware MrElectronics

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" data-teheme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="navbar-start">
+            <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+                tabindex="0"
+                class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li><a>Inventario</a></li>
+                <li><a>Ventas</a></li>
+            </ul>
+            </div>
+            <a class="btn btn-ghost text-xl">MR ELECTRONICS</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+            <ul class="menu menu-horizontal px-1">
+                <li><a>Inventario</a></li>
+                <li><a>Ventas</a></li>
+            </ul>
+        </div>
+        <div class="navbar-end">
+            <a class="btn">Log Out</a>
+        </div>
+    </div>
+
+    <div class="p-6">
+        @yield('content')
+    </div>
+
+</body>
+</html>
+
