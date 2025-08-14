681665846f48f9da6bf7a6c0471dc81fa6fd837a
luis escobar <luisescobar74628191@gmail.com>
2025-08-14 10:22:07 -0500

    creando migraciones

---
+++ database/migrations/2025_08_14_151602_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('marca_id')->constrained('marcas')->onDelete('cascade');
+            $table->foreignId('modelo_id')->constrained('modelos')->onDelete('cascade');
+            $table->decimal('precio', 12, 2);
+            $table->integer('cantidad');
+            $table->string('numero_pieza', 100)->nullable();
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
